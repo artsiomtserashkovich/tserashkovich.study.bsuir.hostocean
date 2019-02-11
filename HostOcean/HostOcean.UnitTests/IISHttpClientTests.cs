d83b2b83aa3c7d51824109f7bcea88229ed71f6c
Артём Терешкович <artyomt7777@gmail.com>
2019-02-11 11:55:25 +0300

    Implement IIS Http Client for get groups list

---
+++ HostOcean/HostOcean.UnitTests/IISHttpClientTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HostOcean.UnitTests
+{
+    public class IISHttpClientTests
+    {               
+        [Fact]
+        public void Test1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
