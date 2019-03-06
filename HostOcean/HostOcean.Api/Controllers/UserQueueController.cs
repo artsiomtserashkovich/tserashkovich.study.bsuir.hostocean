12cef9e1e24480119f0441c5b940303d59858fb5
Vadim Rakevich <vrokez@gmail.com>
2019-03-07 02:15:53 +0300

    UserQueueController added

---
+++ HostOcean/HostOcean.Api/Controllers/UserQueueController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HostOcean.Application.Queue.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HostOcean.Api.Controllers
+{
+    public class UserQueueController : BaseController
+    {
+        private readonly IMapper _mapper;
+
+        public UserQueueController(IMediator mediator, IMapper mapper) : base(mediator)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpPost("take")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Take([FromBody]TakeQueueCommand command)
+        {
+            var result = await Mediator.Send(command);
+
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result.Error);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ HostOcean/HostOcean.Application/Queue/Commands/TakeQueueCommand.cs
@@ -0,0 +1,51 @@
+﻿using HostOcean.Domain.Entities;
+using HostOcean.Infrastructure;
+using HostOcean.Persistence.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostOcean.Application.Queue.Commands
+{
+    public class TakeQueueCommand : IRequest<ServiceResult>
+    {
+        public string UserId { get; set; }
+        public string QueueId { get; set; }
+        public short Order { get; set; }
+
+        public class Handler : IRequestHandler<TakeQueueCommand, ServiceResult>
+        {
+            private readonly IMediator _mediator;
+            private readonly IUnitOfWork _unitOfWork;
+
+            public Handler(IMediator mediator, IUnitOfWork unitOfWork)
+            {
+                _mediator = mediator;
+                _unitOfWork = unitOfWork;
+            }
+
+            public async Task<ServiceResult> Handle(TakeQueueCommand request, CancellationToken cancellationToken)
+            {
+                var entity = new UserQueue
+                {
+                    UserId = request.UserId,
+                    QueueId = request.QueueId,
+                    Order = request.Order
+                };
+
+                try
+                {
+                    _unitOfWork.UserQueues.Add(entity);
+                    await _unitOfWork.SaveAsync();
+
+                    return new ServiceResult();
+                }
+                catch (Exception ex)
+                {
+                    return new ServiceResult(ex);
+                }
+            }
+        }
+    }
+}
