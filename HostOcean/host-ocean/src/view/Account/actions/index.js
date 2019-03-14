f4d87433c6644b287d84616cf4c417961095f4cf
Vadim Rakevich <vrokez@gmail.com>
2019-03-15 01:26:14 +0300

    Typescript files removed, some folders renamed

--- HostOcean/host-ocean/src/view/Account/actions/index.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'redux-actions'
-
-export const getPingRequest = createAction('GET_PING_REQUEST')
-export const getPingSuccess = createAction('GET_PING_SUCCESS')
-export const getPingFailed = createAction('GET_PING_FAILED')
