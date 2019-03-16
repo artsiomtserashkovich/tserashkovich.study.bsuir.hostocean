e33440d0275a3334cc48597d623855a0dff4e9ec
Vadim Rakevich <vrokez@gmail.com>
2019-03-16 21:59:13 +0300

    Add CalendarId

---
+++ HostOcean/HostOcean.Persistence/Migrations/20190316185733_Add_CalendarId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HostOcean.Persistence.Migrations
+{
+    public partial class Add_CalendarId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CalendarId",
+                table: "Groups",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CalendarId",
+                table: "Groups");
+        }
+    }
+}
