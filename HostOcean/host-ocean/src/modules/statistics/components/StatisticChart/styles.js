27fc7230fbecf2faad90ad6d03cbcf216f7fe39c
ITRANSITION\a.tereshkovich <a.tereshkovich@itransition.com>
2019-05-04 15:40:13 +0300

    refactor statistic chart

--- HostOcean/host-ocean/src/modules/statistics/components/StatisticChart/styles.js
+++ HostOcean/host-ocean/src/modules/statistics/components/StatisticChart/styles.js
@@ -1,12 +1,3 @@
-const styles = theme => ({
-    paper: {
-        marginTop: theme.spacing.unit,
-        marginBot: theme.spacing.unit,
-    },
-    chartContainetDiv: {
-        width: "100%", 
-        height: 500
-    }
-});
-  
+const styles = theme => ({});
+
 export default styles;
--- HostOcean/host-ocean/src/modules/statistics/containers/StatisticsContainer.js
+++ HostOcean/host-ocean/src/modules/statistics/containers/StatisticsContainer.js
@@ -15,7 +15,69 @@ export class StatisticsContainer extends React.Component {
                     <StatisticForm/>
                 </Grid>
                 <Grid item>
-                    <StatisticChart/>
+                    <StatisticChart
+                        chartId={"testChart"}
+                        colorSchema={"#722452"}
+                        seriesDataField={"queueStartTime"}
+                        seriesValueField={"place"}
+                        seriesTooltipTextPattern={"{queueStartTime}:{place}; TakeTime:{takeQueueTime}"}
+                        data={
+                            [
+                                {place: 3, queueStartTime: "2019-05-01", takeQueueTime: "0.2h"},
+                                {place: 2, queueStartTime: "2019-05-02", takeQueueTime: "0.2h"},
+                                {place: 1, queueStartTime: "2019-05-04", takeQueueTime: "0.2h"},
+                                {place: 4, queueStartTime: "2019-05-08", takeQueueTime: "0.2h"},
+                                {place: 2, queueStartTime: "2019-05-10", takeQueueTime: "0.2h"},
+                                {place: 10,queueStartTime: "2019-05-20", takeQueueTime: "0.2h"},
+                                {place: 8, queueStartTime: "2019-05-28", takeQueueTime: "0.2h"},
+                                {place: 5, queueStartTime: "2019-05-30", takeQueueTime: "0.2h"},
+                                {place: 3, queueStartTime: "2019-06-01", takeQueueTime: "0.2h"}
+                            ]
+                        }
+
+                    />
+                    <StatisticChart
+                        chartId={"testChart1"}
+                        colorSchema={"#4c6859"}
+                        seriesDataField={"queueStartTime"}
+                        seriesValueField={"place"}
+                        seriesTooltipTextPattern={"{queueStartTime}:{place}; TakeTime:{takeQueueTime}"}
+                        data={
+                            [
+                                {place: 3, queueStartTime: "2019-05-01", takeQueueTime: "0.2h"},
+                                {place: 2, queueStartTime: "2019-05-02", takeQueueTime: "0.2h"},
+                                {place: 1, queueStartTime: "2019-05-04", takeQueueTime: "0.2h"},
+                                {place: 4, queueStartTime: "2019-05-08", takeQueueTime: "0.2h"},
+                                {place: 2, queueStartTime: "2019-05-10", takeQueueTime: "0.2h"},
+                                {place: 10,queueStartTime: "2019-05-20", takeQueueTime: "0.2h"},
+                                {place: 8, queueStartTime: "2019-05-28", takeQueueTime: "0.2h"},
+                                {place: 5, queueStartTime: "2019-05-30", takeQueueTime: "0.2h"},
+                                {place: 3, queueStartTime: "2019-06-01", takeQueueTime: "0.2h"}
+                            ]
+                        }
+
+                    />
+                    <StatisticChart
+                        chartId={"testChart2"}
+                        colorSchema={"#554b63"}
+                        seriesDataField={"queueStartTime"}
+                        seriesValueField={"place"}
+                        seriesTooltipTextPattern={"{queueStartTime}:{place}; TakeTime:{takeQueueTime}"}
+                        data={
+                            [
+                                {place: 3, queueStartTime: "2019-05-01", takeQueueTime: "0.2h"},
+                                {place: 2, queueStartTime: "2019-05-02", takeQueueTime: "0.2h"},
+                                {place: 1, queueStartTime: "2019-05-04", takeQueueTime: "0.2h"},
+                                {place: 4, queueStartTime: "2019-05-08", takeQueueTime: "0.2h"},
+                                {place: 2, queueStartTime: "2019-05-10", takeQueueTime: "0.2h"},
+                                {place: 10,queueStartTime: "2019-05-20", takeQueueTime: "0.2h"},
+                                {place: 8, queueStartTime: "2019-05-28", takeQueueTime: "0.2h"},
+                                {place: 5, queueStartTime: "2019-05-30", takeQueueTime: "0.2h"},
+                                {place: 3, queueStartTime: "2019-06-01", takeQueueTime: "0.2h"}
+                            ]
+                        }
+
+                    />
                 </Grid>
             </Grid>
         );
